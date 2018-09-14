08b81cc47b144d132540356108566d7290b2f11a
Yospeh <yadugna@gmail.com>
2018-09-13 22:53:24 -0700

    User Model

--- DebeloApp.API/Data/DataContext.cs
+++ DebeloApp.API/Data/DataContext.cs
@@ -3,14 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DebeloApp.API.Data
 {
-    public class DataContext:DbContext 
+    public class DataContext : DbContext
     {
+         
+
         public DataContext(DbContextOptions<DataContext> options)
-        :base(options){}
-        public DbSet<Value> Values {get;set;}
-    }  
-            
-       
-    
-    
+: base(options) { }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
+
+
+
+
+}
---
+++ DebeloApp.API/Models/User.cs
@@ -0,0 +1,10 @@
+namespace DebeloApp.API.Models
+{
+    public class User
+    {
+       public int Id { get; set; } 
+       public string Username { get; set; }
+       public byte[] PasswordHas { get; set; }
+       public byte[] PasswordSalt{ get; set; }
+    }
+}
